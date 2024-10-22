b827fafe73cbdbc5f793c558a6a447dcc0b6c497
gayatrisoni <gayatrisoni613@gmail.com>
2024-10-21 23:39:08 -0700

    set up the cotroller and routes for trips table

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import tripRoutes from './routes/trips.js'
 
 // create express app
 const app = express()
@@ -13,6 +14,8 @@ const PORT = process.env.PORT || 3001
 app.get('/', (req, res) => {
     res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">✈️ On the Fly API</h1>')
 })
+
+app.use('/trips', tripRoutes)
 
 
 
